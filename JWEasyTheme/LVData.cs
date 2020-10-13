c80c7bd4d274f77f19fe71e20defc84c25c5c3de
groovykool <48837150+groovykool@users.noreply.github.com>
2020-10-13 13:36:13 -0500

    Add project files.

---
+++ JWEasyTheme/LVData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWEasyTheme
+{
+    class LVData : INotifyPropertyChanged
+    {
+        //public string Bname;
+
+        private string bname;
+        public string Bname
+        {
+            get { return bname; }
+            set { Set(ref bname, value, "Bname"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
