e7e5198616ba32c9d735659d957c6e41378671da
Anthony Wilkey <groovykool@gmail.com>
2021-03-04 13:39:52 -0600

    Tweaks and tests

---
+++ JWEasyTheme/Controls/BetterGrid.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace JWEasyTheme.Controls
+{
+    [ContentProperty(Name = "Children")]
+    public sealed class BetterGrid : Control
+    {
+        public static readonly DependencyProperty ChildrenProperty = DependencyProperty.Register(
+          nameof(Children),
+          typeof(UIElementCollection),
+          typeof(BetterGrid),
+          new PropertyMetadata(default));
+
+        public UIElementCollection Children
+        {
+            get { return (UIElementCollection)GetValue(ChildrenProperty); }
+            private set { SetValue(ChildrenProperty, value); }
+        }
+        public BetterGrid()
+        {
+            this.DefaultStyleKey = typeof(BetterGrid);               
+        }     
+    }
+}
---
+++ JWEasyTheme/Controls/ThemeGrid.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace JWEasyTheme.Controls
+{
+    [ContentProperty(Name = "Children")]
+    public sealed partial class ThemeGrid : UserControl
+    {
+        public static readonly DependencyProperty ChildrenProperty = DependencyProperty.Register(
+           nameof(Children),
+           typeof(UIElementCollection),
+           typeof(ThemeGrid),
+           new PropertyMetadata(default));
+
+        public UIElementCollection Children
+        {
+            get { return (UIElementCollection)GetValue(ChildrenProperty); }
+            private set { SetValue(ChildrenProperty, value); }
+        }
+
+
+        public ThemeGrid()
+        {
+            this.InitializeComponent();
+            Children = BaseG.Children;
+        }
+    }
+}
